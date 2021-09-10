4587fa2cda9dcd8862b51932d6d84b8a06af4898
Huscskn <huseyin.coskun1996@gmail.com>
2021-09-10 14:15:02 +0300

    Socket added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const expressEdge = require("express-edge");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const http = require('http');
 require('dotenv').config();
 
 const createPostController = require('./controllers/createPost');
@@ -12,6 +13,16 @@ const createUserController = require("./controllers/createUser");
 const storeUserController = require('./controllers/storeUser');
 
 const app = new express();
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST","PUT","DELETE","HEAD","PATCH"],
+    allowedHeaders: ["Origin, X-Requested-With, Content-Type, Accept, Authorization, X-CSRF-Token, User-Agent"],
+    credentials: true,
+  }
+});
 
 mongoose.connect(process.env.MongoDB, { useNewUrlParser: true })
     .then(() => 'You are now connected to Mongo!')
@@ -33,6 +44,13 @@ app.post("/posts/store", storePostController);
 app.get("/auth/register", createUserController);
 app.post("/users/register", storeUserController);
 
-app.listen(4000, () => {
+io.on('connection', (socket) => {
+  console.log("a user connected")
+  socket.on('new post', msg => {
+    io.emit('new post', msg);
+  });
+});
+
+server.listen(4000, () => {
   console.log("App listening on port 4000");
 });
